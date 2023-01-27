101abfb008c09e4f01f31f1bbc2a5e363208a050
min0731 <ypd04208@gmail.com>
2023-01-27 23:45:24 +0900

    Data_str_01_27

--- Chapter6_Sort/page213_Straight_Selection_Sort.java
+++ Chapter6_Sort/page213_Straight_Selection_Sort.java
@@ -4,50 +4,60 @@ import java.util.Arrays;
 
 public class page213_Straight_Selection_Sort {
 
-    static void selctionSort(int [] arr,int n){
+    static void selctionSort(Integer[] arr, int n) {
 
         int max = 0;
-        
-        for(int i=0;i<n;i++){
-            if(arr[i]>max){
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
 
-        System.out.println("최댓값: "+ max);
-        for(int i=0;i<n;i++){
+        System.out.println("최댓값: " + max);
+
+        for (int i = 0; i < n - 1; i++) {
             int min = max;
-            int now_min = 0;
-            System.out.println(i + "번째");
-            for(int j=i;j<n;j++){
-                if(arr[j]<min){
-                    System.out.println("min 변경"+arr[j]);
-                    now_min = arr[j];
+            for (int j = i; j < n; j++) {
+                if (arr[j] <= min) {
+                    min = arr[j];
                 }
             }
-            arr[i] = now_min;
+
+            System.out.println("정렬 전: " + Arrays.toString(arr));
+            System.out.println(i + 1 + "번째 최솟값: " + min);
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] == arr[Arrays.asList(arr).indexOf(min)]) {
+                    System.out.println("최솟값의 인덱스: " + j);
+                    System.out.println(Arrays.asList(arr).indexOf(min) + " <-> " + i);
+                    swap(arr, j, i);
+                    break;
+                }
+            }
+            swap(arr, Arrays.asList(arr).indexOf(min), i);
+            System.out.println("정렬 후: " + Arrays.toString(arr));
+            System.out.println("--------------");
         }
-        
+
     }
 
-    static void swap(int [] arr,int a, int b){
-        
+    static void swap(Integer[] arr, int a, int b) {
+
         int temp = arr[a];
         arr[a] = arr[b];
-        arr[b] = arr[a];
+        arr[b] = temp;
 
     }
 
     public static void main(String[] args) {
 
-        int [] arr = new int [] {3,4,2,3,1};
+        Integer[] arr = { 3, 4, 2, 3, 1 };
 
         System.out.println(Arrays.toString(arr));
 
-        selctionSort(arr,arr.length);
+        selctionSort(arr, arr.length);
 
-        System.out.println(Arrays.toString(arr));
-        
     }
-    
+
 }
